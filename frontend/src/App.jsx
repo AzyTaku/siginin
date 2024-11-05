df840c6546ebc07b571c6001e91c3c999d8677f1
AzyTaku <azlanshaveenraban1@gmail.com>
2024-11-05 12:54:46 +0530

    Notes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import SignIn from "./pages/SignIn.jsx";
+import Notes from "./pages/Notes.jsx";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
 
-  return (
-    <>
-      <SignIn />
-    </>
-  )
-}
+const App = () => (
+  <Router>
+    <div className="bg-secondary w-full overflow-hidden">
+      <Routes>
+        <Route path="/" element={
+          <>
+            <SignIn />
+          </>
+        } />
+        <Route path="/Notes" element={<Notes />} />
+      </Routes>
+    </div>
+  </Router>
+);
 
 export default App
---
+++ frontend/src/pages/Notes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/login.css';
+
+const Notes = () => {
+    return (
+        <div className='text-white krona'>
+            Notes Will be Displayed here!
+        </div>
+    )
+}
+
+export default Notes
